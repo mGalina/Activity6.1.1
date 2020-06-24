b7ed43f11317841916749a21045af1712d002ef0
Galina Marakina <g.marakina.2gis@gmail.com>
2020-06-24 20:27:45 +0300

    Add saveText

--- app/src/main/java/com/example/activity611/MainActivity.java
+++ app/src/main/java/com/example/activity611/MainActivity.java
@@ -13,72 +13,74 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mText;
     private static final String LOG_TAG = "MyLogs";
+    private String text ="undefined";
+    final static String nameVariableKey = "NAME_VARIABLE";
+    final static String textViewTexKey = "TEXTVIEW_TEXT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Log.d(LOG_TAG, getString(R.string.onCreate));
-        mText.append("\n" + getString(R.string.onCreate));
 
         mText = findViewById(R.id.tv_hello);
     }
 
     @Override
     protected void onStart() {
+        super.onStart();
+        mText.setText(text);
         Log.d(LOG_TAG, getString(R.string.onStart));
         mText.append("\n" + getString(R.string.onStart));
-        super.onStart();
     }
 
     @Override
     protected void onResume() {
+        super.onResume();
         Log.d(LOG_TAG, getString(R.string.onResume));
         mText.append("\n" + getString(R.string.onResume));
-        super.onResume();
     }
 
     @Override
     protected void onPause() {
+        super.onPause();
         Log.d(LOG_TAG, getString(R.string.onPause));
         mText.append("\n" + getString(R.string.onPause));
-        super.onPause();
     }
 
     @Override
     protected void onStop() {
+        super.onStop();
         Log.d(LOG_TAG, getString(R.string.onStop));
         mText.append("\n" + getString(R.string.onStop));
-        super.onStop();
     }
 
     @Override
     protected void onDestroy() {
+        super.onDestroy();
         Log.d(LOG_TAG, getString(R.string.onDestroy));
         mText.append("\n" + getString(R.string.onDestroy));
-        super.onDestroy();
     }
 
     @Override
     protected void onRestart() {
+        super.onRestart();
         Log.d(LOG_TAG, getString(R.string.onRestart));
         mText.append("\n" + getString(R.string.onRestart));
-        super.onRestart();
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
         Log.d(LOG_TAG, getString(R.string.onPostCreate));
         mText.append("\n" + getString(R.string.onPostCreate));
-        super.onPostCreate(savedInstanceState);
     }
 
     @Override
     protected void onPostResume() {
+        super.onPostResume();
         Log.d(LOG_TAG, getString(R.string.onPostResume));
         mText.append("\n" + getString(R.string.onPostResume));
-        super.onPostResume();
     }
 
     @Override
@@ -97,22 +99,27 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
+        super.onBackPressed();
         Log.d(LOG_TAG, getString(R.string.onBackPressed));
         mText.append("\n" + getString(R.string.onBackPressed));
-        super.onBackPressed();
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(nameVariableKey, text);
+        outState.putString(textViewTexKey, mText.getText().toString());
+        super.onSaveInstanceState(outState);
         Log.d(LOG_TAG, getString(R.string.onSaveInstanceState));
         mText.append("\n" + getString(R.string.onSaveInstanceState));
-        super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        text = savedInstanceState.getString(nameVariableKey);
+        String textViewText= savedInstanceState.getString(textViewTexKey);
+        mText.setText(textViewText);
         Log.d(LOG_TAG, getString(R.string.onRestoreInstanceState));
         mText.append("\n" + getString(R.string.onRestoreInstanceState));
-        super.onRestoreInstanceState(savedInstanceState);
     }
 }
